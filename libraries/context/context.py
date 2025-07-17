d7bb44aeeefbf67997158710f5845d3ea041c401
Jay Looty <jaylooty.official@gmail.com>
2025-07-17 11:41:02 +0200

    fix(about): Create dialog About

--- libraries/context/context.py
+++ libraries/context/context.py
@@ -97,6 +97,7 @@ class Context:
 
         # Initialize selenium's web browser
         chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--log-level=3")
         chrome_options.add_experimental_option(
             "excludeSwitches", ["enable-logging"]
